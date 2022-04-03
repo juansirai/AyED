8b06617fcfc46394735af139cb97c401c6a63c9a
juans <juanbsirai664@gmail.com>
2022-04-03 14:51:02 -0300

    clases de pila y cola generica

---
+++ 3 - Semana 3/Practica/ListasGenericas/src/tp02/ejercicio2/PilaGenerica.java
@@ -0,0 +1,24 @@
+package tp02.ejercicio2;
+
+public class PilaGenerica<T> {
+	ListaGenerica<T> datos = new ListaEnlazadaGenerica<T>();
+	
+	public boolean Apilar(T elemento) {
+		this.datos.agregarInicio(elemento);
+		return true;
+	}
+	
+	public T Tope(){
+		return this.datos.elemento(1);
+	}
+	
+	public T Desapilar() {
+		T elemento = this.Tope();
+		this.datos.eliminarEn(1);
+		return elemento;
+	}
+	
+	public boolean esVacia() {
+		return this.datos.esVacia();
+	}
+}
